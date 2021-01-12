78f19afeca19e599783c912e6b0f7883ee90ddbd
Ethan Lam <ethan.lam@mail.utoronto.ca>
2021-01-12 01:21:19 -0500

    finished mockup of math interface

--- pygametest.py
+++ pygametest.py
@@ -1,12 +1,34 @@
 import pygame
-
-
 pygame.init()
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 500
+font = pygame.font.Font('freesansbold.ttf', 32)
+green = (0, 255, 0)
+blue = (0, 0, 128)
 
-win = pygame.display.set_mode(((500, 500)))
+win = pygame.display.set_mode(((WINDOW_WIDTH, WINDOW_HEIGHT)))
 
 pygame.display.set_caption("Test")
 
+test_string = "(2 + 3) + 86 + 17/ 5 = 1"
+num_spaces = len(test_string)
+equation_display = []
+
+for i in range(num_spaces):
+    if test_string[i] == "+" or test_string[i] == "-" or test_string[i] == "/" or test_string[i] == "*":
+        equation_display.append(((20 + i*(WINDOW_WIDTH - 50)/num_spaces), WINDOW_HEIGHT*0.30, 30, 30))
+    elif test_string[i].isnumeric() or test_string[i] in "()=": 
+        num = font.render(test_string[i], True, green, blue)
+        rect = num.get_rect()
+        rect.topleft = ((20 + i*(WINDOW_WIDTH - 50)/num_spaces),
+                       WINDOW_HEIGHT*0.30)
+        equation_display.append((num, rect))
+
+
+mult = mult_x, mult_y, symbol_width, symbol_height = (50, WINDOW_HEIGHT*0.70, 30, 30)
+add = add_x, add_y, symbol_width, symbol_height = ((50 + (WINDOW_WIDTH - 50)/4), WINDOW_HEIGHT*0.70, 30, 30)
+subtract = subtract_x, subtract_y, symbol_width, symbol_height = ((50 + 2*(WINDOW_WIDTH - 50)/4), WINDOW_HEIGHT*0.70, 30, 30)
+divide = divide_x, divide_y, symbol_width, symbol_height = ((50 + 3*(WINDOW_WIDTH - 50)/4), WINDOW_HEIGHT*0.70, 30, 30)
 
 x = 50
 y = 50
@@ -26,7 +48,7 @@ while run:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1 and x <= mx <= x + width and y <= my <= my + height:
-                clicking_rect = True
+                clicking_mult = True
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 clicking_rect = False
@@ -35,7 +57,18 @@ while run:
         x = mx
         y = my
     pygame
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    # pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    for item in equation_display:
+        if len(item) == 2:
+            win.blit(item[0], item[1])
+        else:
+            pygame.draw.rect(win, (255, 0, 0), item)
+    
+    pygame.draw.rect(win, green, mult)
+    pygame.draw.rect(win, green, add)
+    pygame.draw.rect(win, green, subtract)
+    pygame.draw.rect(win, green, divide)
+
     pygame.display.update()
 
 
