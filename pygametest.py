a80faf773aaaaac754d6e291cf9c8f1b90d41be7
rehmat10 <61368086+rehmat10@users.noreply.github.com>
2021-01-13 23:48:05 -0500

    Added check button to make string out of selected string of operations

--- pygametest.py
+++ pygametest.py
@@ -22,18 +22,28 @@ mult = pygame.transform.scale(times, (30, 30))
 divide = pygame.transform.scale(divide, (30, 30))
 
 test_string = "(2 + 3) + 86 + 17 / 5 = 1"
+omited_string = ""
+final_string = ""
+
 num_spaces = len(test_string)
 equation_display = []
 
 for i in range(num_spaces):
     if test_string[i] == "+" or test_string[i] == "-" or test_string[i] == "/" or test_string[i] == "*":
         equation_display.append(((20 + i*(WINDOW_WIDTH - 50)/num_spaces), WINDOW_HEIGHT*0.30, 30, 30))
+        omited_string += '#'
     elif test_string[i].isnumeric() or test_string[i] in "()=": 
         num = font.render(test_string[i], True, blue, pale)
         rect = num.get_rect()
         rect.topleft = ((20 + i*(WINDOW_WIDTH - 50)/num_spaces),
                        WINDOW_HEIGHT*0.30)
         equation_display.append((num, rect))
+        omited_string += test_string[i]
+    else:
+        omited_string += test_string[i]
+
+print(omited_string)
+
 
 
 mult_l = mult_x, mult_y = [50, WINDOW_HEIGHT*0.70]
@@ -48,7 +58,7 @@ divide_orig = (divide_x, divide_y)
 
 
 signs = []
-
+signs_placed = []
 
 x = 50
 y = 50
@@ -70,23 +80,36 @@ while run:
             if event.button == 1:
                 if mult_orig[0] <= mx <= mult_orig[0] + 50 and mult_orig[1] <= my <= mult_orig[1] + 50:
                     new_mult = mult
-                    signs.append([new_mult, mx, my, "*"])
+                    signs.append([new_mult, mx, my, "*", False])
                     clicking_obj[len(signs) - 1] = True
 
                 if minus_orig[0] <= mx <= minus_orig[0] + 50 and minus_orig[1] <= my <= minus_orig[1] + 50:
                     new_minus = minus
-                    signs.append([new_minus, mx, my, "-"])
+                    signs.append([new_minus, mx, my, "-", False])
                     clicking_obj[len(signs) - 1] = True
 
                 if plus_orig[0] <= mx <= plus_orig[0] + 50 and plus_orig[1] <= my <= plus_orig[1] + 50:
                     new_plus = plus
-                    signs.append([new_plus, mx, my, "+"])
+                    signs.append([new_plus, mx, my, "+", False])
                     clicking_obj[len(signs) - 1] = True
 
                 if divide_orig[0] <= mx <= divide_orig[0] + 50 and divide_orig[1] <= my <= divide_orig[1] + 50:
                     new_divide = divide
-                    signs.append([new_divide, mx, my, "/"])
+                    signs.append([new_divide, mx, my, "/", False])
                     clicking_obj[len(signs) - 1] = True
+
+                if mx >= 450 and my >= 450:
+                    signs_placed.sort()
+                    final_string = omited_string
+                    for i in signs_placed:
+                        final_string = final_string.replace("#", i[1], 1)
+                    if "#" in final_string:
+                        print("Put an operator in EVERY red box")
+                    else:
+                        print(final_string)
+
+
+
 
             for i in range(len(signs)):
                 if signs[i][1] < mx < signs[i][1] + 50 and signs[i][2] < my < signs[i][2] + 50:
@@ -100,6 +123,7 @@ while run:
                 for i in clicking_obj:
                     if clicking_obj[i]:
                         clicking_obj[i] = False
+                        is_on_blank = False
                         for y in range(len(equation_display)):
                             if len(equation_display[y]) != 2:
                                 blank_loc = (equation_display[y][0], equation_display[y][1])
@@ -109,8 +133,20 @@ while run:
                                     signs[i][2] = blank_loc[1]
                                     if signs[i][3] == '-':
                                         signs[i][2] += 12
-
-
+                                    if signs[i][4]:
+                                        for z in range(len(signs_placed)):
+                                            if signs_placed[z][2] == i:
+                                                signs_placed.pop(z)
+                                                break
+                                    signs_placed.append((y, signs[i][3], i))
+                                    is_on_blank = True
+                        if signs[i][4] and not is_on_blank:
+                            signs[i][4] = False
+                            for y in range(len(signs_placed)):
+                                if signs_placed[y][2] == i:
+                                    signs_placed.pop(y)
+                                    break
+                        signs[i][4] = is_on_blank
                         break
 
 
@@ -142,6 +178,9 @@ while run:
     win.blit(divide, divide_l)
     #win.blit(minus, minus_l)
     win.blit(mult, mult_l)
+
+    #Check button
+    pygame.draw.rect(win, (0, 0, 0), (450, 450, 50, 50))
 
     pygame.display.update()
 
