eb3eccc5b603b39292abd1cd82f9daf843c63375
rehmat10 <61368086+rehmat10@users.noreply.github.com>
2021-01-15 20:57:12 -0500

    Changed eval_expression to only take in one string that includes the = operator

--- eval_expression.py
+++ eval_expression.py
@@ -1,4 +1,4 @@
-def eval_expression(s1: str, s2: str) -> bool:
+def eval_expression(s1: str) -> bool:
     """
     Evaluates the result of s1 and s2, and returns if they are equal.
     Precondition: s1 and s2 are string representations of a
@@ -6,9 +6,9 @@ def eval_expression(s1: str, s2: str) -> bool:
     Post-condition: returns True if both mathematical expressions are
     equal to each other
     """
-    return eval(s1.split('=')[0]) == eval(s2.split('=')[0])
+    return eval(s1.split('=')[0]) == eval(s1.split('=')[1])
 
 
 if __name__ == "__main__":
-    print(eval_expression("2/1", "(1 + 1 - 1)*2"))
-    print(eval_expression("2/1", "1 + 1 - 1"))
+    print(eval_expression("2 = (1 + 1 - 1)*2"))
+    print(eval_expression("1 + 1 - 1 = 2"))
